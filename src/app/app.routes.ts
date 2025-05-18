14ea2c8202318dab312b593fb605986aafbc1a89
Moayadooh <moayadsalim97@gmail.com>
2025-05-18 15:20:43 +0400

    add second route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from "@angular/router";
 import { TaskComponent } from "./tasks/task/task.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 
 export const routes: Routes = [
+    {
+        path: '', // <your-domain>/tasks
+        component: NoTaskComponent
+    },
     {
         path: 'tasks', // <your-domain>/tasks
         component: TaskComponent
