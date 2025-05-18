b2ca299848293aa264e308c1c806e3852893b72e
Moayadooh <moayadsalim97@gmail.com>
2025-05-18 20:19:55 +0400

    create nested routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from "@angular/router";
-import { TaskComponent } from "./tasks/task/task.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 export const routes: Routes = [
     {
@@ -10,6 +11,16 @@ export const routes: Routes = [
     },
     {
         path: 'users/:userId', // <your-domain>/users/<uid>
-        component: UserTasksComponent
+        component: UserTasksComponent,
+        children: [
+            {
+                path: 'tasks', // <your-domain>/users/<uid>/tasks
+                component: TasksComponent
+            },
+            {
+                path: 'tasks/new',
+                component: NewTaskComponent
+            }
+        ]
     }
 ];
