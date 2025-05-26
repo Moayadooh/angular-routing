62c002870797574b253fede60ae8623a05c838cf
Moayadooh <moayadsalim97@gmail.com>
2025-05-26 16:13:22 +0400

    Resolving Route-related Dynamic Data

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,6 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -9,25 +9,37 @@ import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent implements OnInit {
+export class UserTasksComponent /* implements OnInit */ {
   // userId = input.required<string>();
-  userName = '';
-  private usersService = inject(UsersService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
+  // userName = '';
+  userName = input.required<string>();
+  message = input.required<string>();
+  // private usersService = inject(UsersService);
+  // private activatedRoute = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef);
 
   // userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
 
-  ngOnInit(): void {
-    console.log(this.activatedRoute);
-    console.log(this.activatedRoute.snapshot);
-    console.log(this.activatedRoute.snapshot.paramMap.get('userId'));
-    const subscription = this.activatedRoute.paramMap.subscribe({
-      next: (paramMap) => {
-        this.userName = this.usersService.users.find(u => u.id === paramMap.get('userId'))?.name || ''
-      },
-    });
+  // ngOnInit(): void {
+  //   console.log('Input Data: ' + this.message());
+  //   console.log(this.activatedRoute);
+  //   console.log(this.activatedRoute.snapshot);
+  //   console.log(this.activatedRoute.snapshot.paramMap.get('userId'));
+  //   const subscription = this.activatedRoute.paramMap.subscribe({
+  //     next: (paramMap) => {
+  //       this.userName = this.usersService.users.find(u => u.id === paramMap.get('userId'))?.name || ''
+  //     },
+  //   });
 
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  // }
 }
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot, 
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName = usersService.users.find(u => u.id === activatedRoute.paramMap.get('userId'))?.name || '';
+  return userName;
+};
